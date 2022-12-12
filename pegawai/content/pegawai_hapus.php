942b89c8af0e69cd34d6bd0b112bc66b64d7b50f
killuazoldyckk <90999681+killuazoldyckk@users.noreply.github.com>
2022-12-13 01:23:53 +0700

    Add files via upload

---
+++ pegawai/content/pegawai_hapus.php
@@ -0,0 +1,13 @@
+<?php
+if(!defined('INDEX')) die ("");
+
+if(file_exist("images/$_GET[foto]"))
+    unlink("images/$_GET[foto]");
+$query = mysqli_query($con, "DELETE FROM pekerja WHERE id_pegawai='$_GET[id]' ");
+if ($query){
+    echo "Data berhasil dihapus";
+    echo "<meta http-equiv='refresh' content='1;url=?hal=pegawai'>";
+} else {
+    echo "tidak dapat menyimpan data!<br/>";
+    echo mysqli_error();
+}
